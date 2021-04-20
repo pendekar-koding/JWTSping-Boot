6c3830d51d4c85a627f583e1e9b116da8479b8aa
rizqi.widiantoro <rizqi.widiantoro@bankmandiri.co.id>
2021-04-20 21:34:01 +0700

    first commit

---
+++ src/main/java/com/pendekar/koding/java/JwtSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.pendekar.koding.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pendekar/koding/java/service/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.pendekar.koding.java.service;
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if ("pendekar".equals(username)) {
+            return new User("pendekar", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
+}
